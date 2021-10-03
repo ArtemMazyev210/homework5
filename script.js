6cb11825ed762c8af37b6ee72b2cfa5243133090
Satira <90534931+ArtemMazyev210@users.noreply.github.com>
2021-10-04 01:20:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById("button");
     button.onclick = function calc() {
-        const cost = document.getElementById('cost').value,
-            number = document.getElementById('number').value,
-            res = document.getElementById('result');
+
+        const cost = document.getElementById('cost').value;
+        const number = document.getElementById('number').value;
+        const res = document.getElementById('result');
+
         if (parseInt(cost) * parseInt(number) > 0)
             res.innerHTML = `Итоговая стоимость: ${parseInt(cost) * parseInt(number)}`;
         else
